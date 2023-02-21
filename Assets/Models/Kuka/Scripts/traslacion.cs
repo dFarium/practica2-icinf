5ce1680ede1b6c75a63a81ded4d87ae2193f1387
Alejandro Torres <ale.torres.jofre@gmail.com>
2023-02-21 19:17:08 -0300

    Añade script para obtener coordenadas xyz para poder trasladar a futuro el robot

--- Assets/Models/Kuka/Scripts/traslacion.cs
+++ Assets/Models/Kuka/Scripts/traslacion.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class traslacion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject cubito;
+    public Vector3 componente;
+    public float x, y, z;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        componente = cubito.GetComponent<Transform>().eulerAngles;
+        x = GetObjectRotationX();
+        y = GetObjectRotationY();
+        z = GetObjectRotationZ();
+
+    }
+
+    float GetObjectRotationZ()
+    {
+        if (componente.z > 180)
+        {
+            return componente.z - 360;
+        }
+        else
+        {
+            return componente.z;
+        }
+    }
+
+    float GetObjectRotationX()
+    {
+        if (componente.x > 180)
+        {
+            return componente.x - 360;
+        }
+        else
+        {
+            return componente.x;
+        }
+    }
+
+    float GetObjectRotationY()
+    {
+        if (componente.y > 180)
+        {
+            return componente.y - 360;
+        }
+        else
+        {
+            return componente.y;
+        }
     }
 }
