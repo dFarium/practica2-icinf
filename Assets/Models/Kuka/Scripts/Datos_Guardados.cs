b3fa4458bf06f793ac36d5aa9e65fce4f7e890c8
Alejandro Torres <ale.torres.jofre@gmail.com>
2023-02-22 19:57:58 -0300

    Añade scripts de guardado y cargado de posiciones del kuka

---
+++ Assets/Models/Kuka/Scripts/Datos_Guardados.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Datos_Guardados : MonoBehaviour
+{
+    //Creamos las variables de rotacion de cada pieza del brazo robot
+    public Quaternion BaseFija;
+    public Quaternion BaseMovil;
+    public Quaternion Brazo1;
+}
