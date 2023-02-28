b8265307ef1f24619ffe1311c68bb6e02f130e70
Alejandro Torres <ale.torres.jofre@gmail.com>
2023-02-28 17:20:13 -0300

    Añade nuevo sistema de Save/Load a Scorbot ER-V y reordena carpetas

---
+++ Assets/Models/Scorbot/Scripts/Datos_Guardados_Scorbot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Datos_Guardados_Scorbot
+{
+    //Creamos las variables de rotacion de cada pieza del brazo robot
+    public Quaternion Base;
+    public Quaternion Hombro;
+    public Quaternion Brazo;
+    public Quaternion Antebrazo;
+    public Quaternion ConjuntoMano;
+}
